4e7788e6a3324070c31f373342888cebc2a7fc1e
darioamade <dario_isd@outlook.com>
2020-07-15 16:30:04 +0100

    commit
    added new files

--- js/index.js
+++ js/index.js
@@ -6,3 +6,27 @@ $(document).ready(() => {
     $(".menu").toggleClass("active");
   });
 });
+
+
+
+
+
+const sendBtn = document.querySelector('#sendBtn');
+const messageIn = document.querySelector('#messageIn');
+const messageOut = document.querySelector('#messageOut');
+
+
+sendBtn.addEventListener("click", sendMsg)
+
+  function sendMsg(){
+    let content = messageIn.value
+    //console.log(content);
+    if(content === ''){
+      alert('Please enter Valid value')
+    }
+    else {
+      messageOut.innerHTML = `Message sent!`;
+      messageIn.value = '';
+    }
+   
+  }
